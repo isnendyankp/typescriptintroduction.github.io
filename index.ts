d924956c5e98c120f9c75124678ad1fe84f8bd40
isnendyan <isnendyankp@gmail.com>
2023-05-15 16:37:48 +0700

    Executing Typescript Code: install axios & learn execute typescript code

--- index.ts
+++ index.ts
@@ -5,3 +5,5 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
   console.log(response.data);
 });
+// dont forget npm install axios before started
+// to execute type ts-node index.js(file name)
