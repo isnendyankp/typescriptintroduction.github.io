622f2f5c518555bcb59e8be62469e07e2fd28950
isnendyan <isnendyankp@gmail.com>
2023-05-16 16:08:04 +0700

    Section 1-One Quick Change: seperate variable then response.data again

--- index.ts
+++ index.ts
@@ -2,8 +2,18 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-  console.log(response.data);
+axios.get(url).then((response) => {
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
 // dont forget npm install axios before started
 // to execute type ts-node index.js(file name)
