7154877af8ededd2b87774bf71182bd75cad2736
isnendyan <isnendyankp@gmail.com>
2023-05-17 16:53:32 +0700

    Section 1-Catching Error with Typescript: response.data as Todo

--- index.ts
+++ index.ts
@@ -2,18 +2,32 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-  const todo = response.data;
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `);
 });
 // dont forget npm install axios before started
 // to execute type ts-node index.js(file name)
+// Response.data has properties of:
+// id
+// title
+// completed
+// print value:
+// The Todo with ID: 1
+//  Has a title of: delectus aut autem
+//  Is it finished? false
