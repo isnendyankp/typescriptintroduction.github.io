5b80b7e3f81488f7d626d43e945e526fcbcc53d5
isnendyan <isnendyankp@gmail.com>
2023-05-18 16:17:06 +0700

    Section 1-Catching Error with Typescript: add logTodo

--- index.ts
+++ index.ts
@@ -15,19 +15,16 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id, title, completed) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
 // dont forget npm install axios before started
 // to execute type ts-node index.js(file name)
 // Response.data has properties of:
-// id
-// title
-// completed
-// print value:
-// The Todo with ID: 1
-//  Has a title of: delectus aut autem
-//  Is it finished? false
