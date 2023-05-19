d149983e8b25b7d9cabc0a5636edbd2c6815015a
isnendyan <isnendyankp@gmail.com>
2023-05-19 16:09:16 +0700

    Section 2-Annotations with variables: learning primitive object

---
+++ annotations/variable.ts
@@ -0,0 +1,15 @@
+// Learn primitive type
+let apples: number = 5;
+// : number type value must be a number
+
+let speed: string = 'fast';
+// : string type value must be a string
+
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
