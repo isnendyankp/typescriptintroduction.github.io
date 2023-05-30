25b148abd2de12f45878c322ec766c1a98e7dfa8
isnendyan <isnendyankp@gmail.com>
2023-05-30 16:25:01 +0700

    Section 4-27- inference around functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,4 +1,18 @@
 // create two arguments with type value number
-const add = (a: number, b: number): number => {
-    return a + b;
+// const add = (a: number, b: number): number => {
+//     return a + b;
+// };
+
+// const subtract = (a: number, b: number): number => {
+//   return a - b;
+// };
+
+
+// shorthand
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number) => {
+  return a - b;
 };
