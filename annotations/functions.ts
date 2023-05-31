9bedd0e21c01ba6ba22d44690656d7f5d7a5c63a
isnendyan <isnendyankp@gmail.com>
2023-05-31 16:40:34 +0700

    Section 4-28: learn Void & Never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -25,3 +25,20 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+// no Return value for this function(void)
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// never going to execute the function completely
+const throwError = (message: string): never => {
+    throw new Error(message);
+};
+
+// still do fallback to void becouse we are technically not returning anything and there's just a chance of us never reaching the end of the function
+const throwFalse = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
