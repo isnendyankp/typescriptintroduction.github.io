f75e397e1ef95803658d5a0a3e3b7b83ae5d1aa5
isnendyan <isnendyankp@gmail.com>
2023-06-05 16:31:08 +0700

    S5-32: array type string with square bracket

--- types.ts
+++ types.ts
@@ -3,3 +3,6 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 
 // new date object instances
 const dates = [new Date(), new Date()];
+
+// array type string with square bracket
+const carsByMake: string[][] = [];
