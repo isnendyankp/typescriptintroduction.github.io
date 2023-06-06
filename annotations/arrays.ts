160d97960d52ad5f7e2e038b01d72a41769a290f
isnendyan <isnendyankp@gmail.com>
2023-06-06 16:29:38 +0700

    S5-33: Help with inference when extracting values

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -2,3 +2,8 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
+
+// Why Types Arrays?
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
