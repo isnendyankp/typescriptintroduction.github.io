f4aba1d118df7c06e2400516e2ce91ae4c225807
isnendyan <isnendyankp@gmail.com>
2023-06-06 16:31:02 +0700

    S5-33: Help with 'map'

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -10,3 +10,10 @@ const myCar = carMakers.pop();
 
 // Prevent incompatible values
 carMakers.push(100);
+
+// Help with 'map'
+carMakers.map(
+  (car: string): string => {
+    return car.toUpperCase();
+  }
+);
