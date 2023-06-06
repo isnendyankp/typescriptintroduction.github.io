f9ff9fe6bb25ee91271d6dae0eaa38eacb605222
isnendyan <isnendyankp@gmail.com>
2023-06-06 16:30:30 +0700

    S5-33: Prevent incompatible values

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -7,3 +7,6 @@ const carsByMake: string[][] = [];
 // Help with inference when extracting values
 const car = carMakers[0];
 const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
