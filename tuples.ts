ed530703683e9dd12c3a9cc48bf4eb4ce14b7efb
isnendyan <isnendyankp@gmail.com>
2023-06-08 11:00:11 +0700

    S6-37: add base type order Drink for tuples order

--- tuples.ts
+++ tuples.ts
@@ -5,4 +5,15 @@ const drink = {
 };
 
 // for this case type value string | number | boolean
-const pepsi = ['brown', true, 40];
+// const pepsi = ['brown', true, 40];
+
+// Create specific order of types of elements inside of this tuples
+// for this case type value string | boolean | number 
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
