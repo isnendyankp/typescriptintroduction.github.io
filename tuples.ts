ac5c95b029e1104815db54f1bac38a5ef2a3992e
isnendyan <isnendyankp@gmail.com>
2023-06-09 19:33:18 +0700

    S6-38: Create 2 same type but not same purpose

--- tuples.ts
+++ tuples.ts
@@ -17,3 +17,6 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// Create same type value but not same purpose function
+const carSpecs: [number, number] = [400, 3354];
