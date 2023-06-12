a8b443352937a6ccd732d9d2fd7b2bec17687bd1
isnendyan <isnendyankp@gmail.com>
2023-06-12 16:28:01 +0700

    S7-38: create function with 1 argument is vehicle

--- interfaces.ts
+++ interfaces.ts
@@ -1,6 +1,12 @@
+// 40. Long Type Annotations
 // create variable with 3 type string , number, boolean
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
 };
+
+// create function with 1 argument is vehicle
+const printVehicle = (vehicle) => {
+
+}
