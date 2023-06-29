8b377122110924dfb8c4900fb7bc3a8a439a45c7
isnendyankp <isnendyankp@gmail.com>
2023-06-30 05:16:38 +0700

    s8-50: cr8 constructor on childclass(Car)

--- classes.ts
+++ classes.ts
@@ -10,24 +10,30 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color)
 
-// class Car extends Vehicle {
-//   // override method drive with results on terminal vromm, beep
-//   // private method can only be called by other method in this class
-//   private drive(): void {
-//     console.log('vroom');
-//   }
-//   startDrivingProcess(): void {
-//     // can call drive because on same class(Car)
-//     this.drive();
-//     // call honk from protected methods on different class(Vehicle)
-//     this.honk();
-//   }
-// }
+// Car = childclass
+class Car extends Vehicle {
+  // cr8 constructor on child class
+  // wheels = property/first argumen, number = type
+  constructor(public wheels: number,) {
+    
+  }
+  // override method drive with results on terminal vromm, beep
+  // private method can only be called by other method in this class
+  private drive(): void {
+    console.log('vroom');
+  }
+  startDrivingProcess(): void {
+    // can call drive because on same class(Car)
+    this.drive();
+    // call honk from protected methods on different class(Vehicle)
+    this.honk();
+  }
+}
 
-// // create new keyword car with class Car
-// const car = new Car();
-// // access/call method
-// // access/call method can display with ts-node classes.ts/  namefile.ts
-// // can call startDrivingProcess because property not private
-// car.startDrivingProcess();
-// car.honk();
+// create new keyword car with class Car
+const car = new Car('red');
+// access/call method
+// access/call method can display with ts-node classes.ts/  namefile.ts
+// can call startDrivingProcess because property not private
+car.startDrivingProcess();
+car.honk();
